8d5fcb802ede0e5f38f77d70ee26dc6e99009b22
Mustapha <mustaelhachmimahti@gmail.com>
2023-03-08 20:50:20 +0100

    wip

--- src/components/SettingsComponent.tsx
+++ src/components/SettingsComponent.tsx
@@ -7,26 +7,30 @@ const SettingsComponent = () => {
 
 
     return (
-        <div className="bg-gray-200 rounded-lg p-4">
-            <h1 className="text-3xl font-bold mb-4">Démineur</h1>
-            <div className="mb-4">
-                <label className="mr-2 font-bold" htmlFor="level">Choisir le niveau:</label>
-                <select
-                    name="level"
-                    id="level"
-                    value={level}
-                    onChange={e => setLevel(e.target.value)}
-                    className="border border-gray-400 rounded p-1"
-                >
-                    <option value="easy">Facile</option>
-                    <option value="medium">Moyen</option>
-                    <option value="expert">Expert</option>
-                    <option value="master">Maître</option>
-                </select>
+        <div className="flex justify-center items-center h-screen">
+            <div className="bg-gray-200 rounded-lg p-4">
+                <h1 className="text-3xl font-bold mb-4">Démineur</h1>
+                <div className="mb-4">
+                    <label className="mr-2 font-bold" htmlFor="level">Choisir le niveau:</label>
+                    <select
+                        name="level"
+                        id="level"
+                        value={level}
+                        onChange={e => setLevel(e.target.value)}
+                        className="border border-gray-400 rounded p-1"
+                    >
+                        <option value="easy">Facile</option>
+                        <option value="medium">Moyen</option>
+                        <option value="expert">Expert</option>
+                        <option value="master">Maître</option>
+                    </select>
+                </div>
+                <div className="flex justify-center">
+                    <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                            onClick={() => navigate('/game?level=' + level)}>Lancer la partie
+                    </button>
+                </div>
             </div>
-            <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                    onClick={() => navigate('/game')}>Lancer la partie
-            </button>
         </div>
     );
 
--- src/pages/game/GamePage.tsx
+++ src/pages/game/GamePage.tsx
@@ -1,14 +1,21 @@
 import BoardComponent from "../../components/BoardComponent";
-import React from "react";
+import React, {useEffect} from "react";
+import {useSearchParams} from "react-router-dom";
 
 const GamePage = () => {
+    const [searchParams] = useSearchParams();
+    const level = searchParams.get("level") ? searchParams.get("level") : "easy";
+
+
+    useEffect(() => {
+
+    }, [searchParams]);
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
             <div className="bg-gray-200 rounded-lg p-4">
                 <h1 className="text-3xl font-bold mb-4">Démineur</h1>
-                {/*todo change url*/}
-                <BoardComponent level={"easy"}/>
+                <BoardComponent level={level as string}/>
             </div>
         </div>
 
